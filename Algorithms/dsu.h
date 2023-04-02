6ffa85a853f477312e056a912b83e2bd05b95617
NiskashY <roman.pantus@mail.ru>
2023-04-02 20:30:13 +0300

    refactor dsu.h before BSUIR OPEN

--- Algorithms/dsu.h
+++ Algorithms/dsu.h
@@ -2,45 +2,49 @@
 
 #include <iostream>
 #include <vector>
+#include <numeric>
 
 namespace MintLib {
-	template <class Type>
-	class DSU {
-	public:
-		DSU() = default;
-	
-		DSU(const Type& n) {
-			parent.resize(n);
-			size.resize(n);
-		}
-		
-		void MakeSet(const Type& vertex) {
-			parent[vertex] = vertex;
-			size[vertex] = 1;
-		}
+    class DSU {
+    public:
+        explicit DSU(const size_t& n) {
+            parent.resize(n);
+            size.resize(n, 1);
+            std::iota(parent.begin(), parent.end(), 0);
+        }
 
-		Type FindSet(Type vertex) { // return root of the set, where vertex located;
-			if (vertex == parent[vertex]) {
-				return vertex;
-			}
-			return parent[vertex] = FindSet(parent[vertex]);
-		}
+        void makeSet(const size_t& vertex) {
+            parent[vertex] = vertex;
+            size[vertex] = 1;
+        }
 
-		void JoinSets(Type lhs, Type rhs) { // groups 2 sets into one: prent of root rhs - lhs
-			lhs = FindSet(lhs);
-			rhs = FindSet(rhs);
-			if (lhs != rhs) {
-				if (size[lhs] < size[rhs]) {
-					std::swap(lhs, rhs);
-				}
-				parent[rhs] = lhs; 
-				size[lhs] += size[rhs];
-			}
-		}
+        size_t getRoot(size_t vertex) { // return root of the set, where vertex located;
+            while (vertex != parent[vertex]) {
+                vertex = (parent[vertex] = parent[parent[vertex]]);
+            }
+            return vertex;
+        }
 
-		private:
-		std::vector<Type> parent{};
-		std::vector<size_t> size{};
-	};
+        void merge(size_t lhs, size_t rhs) { // groups 2 sets into one: prent of root rhs - lhs
+            lhs = getRoot(lhs);
+            rhs = getRoot(rhs);
+            if (lhs != rhs) {
+//                if (size[lhs] < size[rhs]) {
+//                    std::swap(lhs, rhs);
+//                }
+                parent[rhs] = lhs;
+                size[lhs] += size[rhs];
+            }
+        }
+
+        size_t getSize(size_t vertex) {
+            return size[getRoot(vertex)];
+        }
+
+    private:
+        std::vector<size_t> parent{};
+        std::vector<size_t> size{};
+    };
 };
+
 
